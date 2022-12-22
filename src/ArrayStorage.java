d4442957d2acfa0a22e646901c150a9a2064b92b
DeryuginVladislav <vladderugin4@gmail.com>
2022-12-22 17:32:54 +0300

    Сделал первое дз basejava

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,62 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int length = size();
+        for (int i = 0; i < length; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            }
+        }
     }
 
     void save(Resume r) {
+        int nullIndex = 0;
+        while (storage[nullIndex] != null) {
+            nullIndex++;
+        }
+        storage[nullIndex] = r;
     }
 
-    Resume get(String uuid) {
-        return null;
+    Resume get(String uuid) throws NullPointerException {
+        try {
+            int counter = 0;
+            while (storage[counter].uuid != uuid) {
+                counter++;
+            }
+            return storage[counter];
+        } catch (NullPointerException e) {
+            return null;
+        }
     }
 
     void delete(String uuid) {
+        int counter = 0;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid == uuid) {
+                storage[i] = null;
+                counter++;
+                break;
+            }
+        }
+        for (int i = --counter; i < size(); i++) {
+            storage[i] = storage[i + 1];
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                size++;
+            }
+        }
+        return size;
     }
 }
