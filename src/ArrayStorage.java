04366f4ad0ed9b0042c01862702446f38384e17e
DeryuginVladislav <vladderugin4@gmail.com>
2022-12-23 17:59:42 +0300

    Исправил get и size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,14 +5,13 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size;
 
     void clear() {
-        int length = size();
-        for (int i = 0; i < length; i++) {
-            if (storage[i] != null) {
-                storage[i] = null;
-            }
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
@@ -21,30 +20,29 @@ public class ArrayStorage {
             nullIndex++;
         }
         storage[nullIndex] = r;
+        size++;
     }
 
-    Resume get(String uuid) throws NullPointerException {
-        try {
-            int counter = 0;
-            while (storage[counter].uuid != uuid) {
-                counter++;
+    Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid) {
+                return storage[i];
             }
-            return storage[counter];
-        } catch (NullPointerException e) {
-            return null;
         }
+        return null;
     }
 
     void delete(String uuid) {
         int counter = 0;
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid == uuid) {
                 storage[i] = null;
                 counter++;
+                size--;
                 break;
             }
         }
-        for (int i = --counter; i < size(); i++) {
+        for (int i = --counter; i < size; i++) {
             storage[i] = storage[i + 1];
         }
     }
@@ -53,16 +51,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        int size = 0;
-        for (Resume resume : storage) {
-            if (resume != null) {
-                size++;
-            }
-        }
         return size;
     }
 }
